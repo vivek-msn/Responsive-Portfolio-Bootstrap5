5d35e0fa775abdfa5e2fa7288a50b246b482e55c
Vivek <vivek_msn@hotmail.com>
2024-03-01 17:25:46 +0530

    fixed nav hide in main.js file

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ window.onscroll = function () {
 
 // Nav Hide 
 let navBar = document.querySelectorAll(".nav-link");
-let navCollapse = dcoument.querySelector(".navbar-collapse.collapse");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
 navBar.forEach(function(a){
     a.addEventListener("click",function(){
         navCollapse.classList.remove("show");
